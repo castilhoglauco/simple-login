20387a549c2d05a4950438078fe40be9e3db2cd2
Glauco de Castilho <castilho.glauco@gmail.com>
2018-09-30 11:26:39 -0300

    Initial commit

---
+++ src/main/java/br/com/glauco/simplelogin/SimpleLoginApplication.java
@@ -0,0 +1,12 @@
+package br.com.glauco.simplelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleLoginApplication.class, args);
+    }
+}
---
+++ src/main/java/br/com/glauco/simplelogin/model/User.java
@@ -0,0 +1,42 @@
+package br.com.glauco.simplelogin.model;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username")
+    private String userName;
+
+    @Column(name = "password")
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
