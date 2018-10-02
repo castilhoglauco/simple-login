5f1e7aefe0390a8d3e45f97bc8263bedc522e7cf
Glauco Carvalho <castilho.glauco@gmail.com>
2018-10-02 09:39:51 -0300

    Alteração final na master.

--- src/main/java/br/com/glauco/simplelogin/SimpleLoginApplication.java
+++ src/main/java/br/com/glauco/simplelogin/SimpleLoginApplication.java
@@ -1,11 +1,7 @@
 package br.com.glauco.simplelogin;
 
-import br.com.glauco.simplelogin.service.UserService;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SimpleLoginApplication {
